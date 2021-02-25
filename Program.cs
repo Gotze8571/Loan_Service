104278e86d506d3369670a0a50ac31a33e1592e3
Tella <tella.adesina@fcmb.com>
2021-02-25 02:26:28 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BBGCombination.Domain
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("The Service is Working!!");
+            Console.ReadLine();
+        }
+    }
+}
