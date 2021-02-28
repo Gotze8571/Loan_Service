b13f0a90017b3c60ca6b6153ff7afab5c8f376e8
Tella <tella.adesina@fcmb.com>
2021-02-28 15:39:08 +0100

    Commit28022021

--- Service/NotifyService.cs
+++ Service/NotifyService.cs
@@ -8,12 +8,24 @@ namespace BBGCombination.Domain.Service
 {
     public class NotifyService
     {
+        public System.Timers.Timer thisTimer;
         public void Start()
         {
             // call Email Sevice
-            //var result = EmailService;
+            thisTimer = new System.Timers.Timer();
+            thisTimer.Enabled = true;
+            int timerInterval = 0;
+            timerInterval = 10800;
+            thisTimer.Interval = timerInterval;
+            thisTimer.AutoReset = true;
+            thisTimer.Elapsed += thistTimer_Tick;
+            thisTimer.Start();
         }
         public void Stop()
+        {
+
+        }
+        private void thistTimer_Tick(System.Object sender, System.EventArgs e)
         {
 
         }
